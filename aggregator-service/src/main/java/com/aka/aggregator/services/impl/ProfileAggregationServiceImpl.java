cf2ed22cbd55777d990eb162443efe3da1e7d2ff
Arnaud Kaninda <73389238+kaninda@users.noreply.github.com>
2026-03-13 09:03:42 +0100

    feat: Refactoring pom for maven (#5)

--- aggregator-service/src/main/java/com/aka/aggregator/services/impl/ProfileAggregationServiceImpl.java
+++ aggregator-service/src/main/java/com/aka/aggregator/services/impl/ProfileAggregationServiceImpl.java
@@ -5,7 +5,6 @@ import com.aka.aggregator.services.ProfileAggregationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
